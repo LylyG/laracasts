78bccf97c9b0b69e891189eb67d22e68891bddc3
Lyrechel Galarza-Punter <lngalarz@ncsu.edu>
2018-03-12 16:30:27 -0400

    Correct the title of lesson 14 to match the lesson name.

---
+++ laracasts/learn_vue_2/lesson_15/main.js
@@ -0,0 +1,40 @@
+//This shared event instance allows components to fire and listen for events. 
+window.Event = new Vue();
+
+
+Vue.component('coupon', {
+
+	template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
+
+	methods: {
+
+		onCouponApplied() {
+
+			Event.$emit('applied');
+
+		}
+	}
+
+});
+
+
+
+new Vue({
+
+	el: '#root',
+
+	data: {
+
+		couponApplied: false
+
+	},
+
+	created() {
+
+		Event.$on('applied', () => alert('Handling it!'));
+
+	}
+
+});
+
+
